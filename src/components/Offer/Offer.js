38409285d14f28aa4358e26e9d59b2a0f465cd14
Mihai Blebea <mihai.blebea@gimo.co.uk>
2017-11-17 16:48:11 +0000

    add typing effect to the main offer

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -10,7 +10,12 @@ function Offer()
             <div className='OfferContent'>
                 <h1 className='display-3 text-center OfferMain'>Do not find time to cook?</h1>
                 <h3 className='text-center mb-5'>
-                    <TypeText>Start building the perfect burger...</TypeText>
+                    <TypeText strings={[
+                        'Start building the perfect burger...',
+                        'Add every ingredient that you like...',
+                        'Fast and secure checkout...',
+                        '...and we will deliver to your door in max 20 mins'
+                    ]}/>
                 </h3>
 
                 <Link to='/burger-builder' style={{textDecoration: 'none'}}>
--- src/components/TypeText/TypeText.js
+++ src/components/TypeText/TypeText.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import './TypeText.css';
+// import './TypeText.css';
+import Typed from 'typed.js';
 
-function TypeText(props)
+class TypeText extends React.Component
 {
-    return (
-        <div className='typewriter'>
-            <p>{props.children}</p>
-        </div>
-    );
+    componentDidMount()
+    {
+        const { strings } = this.props;
+        const options = {
+        	strings: strings,
+            typeSpeed: 50,
+            fadeOut: true,
+            backDelay: 2500,
+            fadeOutDelay: 500,
+            loop: true,
+            loopCount: Infinity,
+        };
+        this.typed = new Typed(this.el, options);
+    }
+
+    componentWillUnmount()
+    {
+        this.typed.destroy();
+    }
+
+    render()
+    {
+        return (
+            <div className="type-wrap">
+                <span
+                    style={{ whiteSpace: 'pre' }}
+                    ref={(el) => { this.el = el; }}/>
+            </div>
+        );
+    }
 }
 
 export default TypeText;
