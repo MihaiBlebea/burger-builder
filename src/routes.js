495d3da478795e2971832769db3316a80f9b3b50
Mihai Blebea <mihai.blebea@gimo.co.uk>
2017-11-17 16:06:14 +0000

    add main front page at /

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Home, BurgerBuilder } from './containers/exports.js';
 
 const routes = [
+    { linkName: 'Home', link: '/', component: Home, show: false },
     { linkName: 'Home', link: '/home', component: Home, show: true },
     { linkName: 'Builder', link: '/burger-builder', component: BurgerBuilder, show: true },
     { linkName: 'Checkout', link: '/checkout', component: Home, show: false },
